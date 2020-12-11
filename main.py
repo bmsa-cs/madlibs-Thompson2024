d1e0f888295185965fbe35a2558b0afc0eb79cc4
Mackenzie Thompson <mthompson24@biomedscienceacademy.org>
2020-12-11 22:09:18 +0000

    finished

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 """
-MadLibs
-Author: 
-Period/Core:
-
+Madlibs
+Author: Mackenzie Thompson
+Period/Core: 2
 
 """
+noun4 = input("what is a noun? ")
+name = input("what is a name? ")
+noun3 = input("what is a noun? ")
+adjective = input("what is an adjective? ")
+noun = input("what is a noun? ")
+noun1 = input("what is a noun? ")
+verb = input("what is a verb? ")
+noun5 = input("what is a noun? ")
+planet = input("What is a planet? ")
+adjective2 = input("what is an adjective? ")
+noun6 = input("what is a noun? ")
+num = input("Give a number(1,10): ")
+noun2 = input("what is a noun? ")
+print( )
+print( )
+
+print(str(name) + " can sense the " + str(planet) 
++ " ending. " + "The main heat source has " + str(adjective) + "  yet again. " + "This time there is nothing we can do to fix it. " + " The " + str(noun) + " has run out of " + str(noun1) + " to emit to keep us warm." + "Everyone has " + str(verb) + " in the local " + str(noun2) + " with all of our " + str(noun3) + "." + " We all huddle together to stay " + str(adjective2) + " ." + "We knew that once the " + str(noun) + " went out for good there was no way to fix it." + "We only had " + str(num) + " minutes left to say our " + str(noun4) + " before we were all frozen to " + str(noun5) + " ." + " We were all " + str(noun6) + " around to see our loved ones for the last time. ")
